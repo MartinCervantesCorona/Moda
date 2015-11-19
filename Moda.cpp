c42e6d543b3497840eba24ee0ee282b6b6977166
MartinCervantesCorona <yigokum@hotmail.com>
2015-11-18 21:22:55 -0600

    moda

--- Moda.cpp
+++ Moda.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main()
 {
-int calificaciones[10],i,rango,mas_grande=0,valor_moda,calt=0,r;
+int calificaciones[10],cal[10],i,rango,mas_grande=0,valor_moda,calt=0,r;
 int frec[10]={0};
 float prom;
 
-	printf("Digite los valores del arreglo:\n");
+	printf("Digite las calificaciones:\n");
 
 	for (i=0;i<10;i++)
 	{
 		scanf("%d",&calificaciones[i]);
+		
 		if(calificaciones[i]>10 || calificaciones[i]<0)
 		{
 		printf("La calificacion no puede ser mayor a 10 ni menor a 0");
@@ -30,6 +31,5 @@ float prom;
 		}
 
 	}
-
 	printf("El valor moda es %d",valor_moda);
 }
